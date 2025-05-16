fdf7fa3e0502fbf78321d3ff2db57310e9284599
Dhiyaa Nazim <dhysdrn@gmail.com>
2025-05-16 08:35:32 -0700

    Added reload button

--- frontend/src/pages/AdminPage.jsx
+++ frontend/src/pages/AdminPage.jsx
@@ -1,10 +1,14 @@
 import DataForm from "../components/DataForm";
 import VectorTable from "../components/VectorTable"; 
+import ReloadButton from "../components/ReloadButton";
+
+
 
 const AdminPage = () => {
     return (
         <div className="admin-page">
           <h1>Admin Dashboard</h1>
+          <ReloadButton />
           <VectorTable />
           <DataForm />
         </div>
