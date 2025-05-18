6973903e9b262dbfb59c32b9c15cba906551fecc
Mason S <sainmason012@gmail.com>
2025-05-17 21:10:49 -0700

    Added both databases to the AI answers

--- backend/connection.js
+++ backend/connection.js
@@ -7,6 +7,7 @@ dotenv.config({
 export const { 
     ASTRA_DB_NAMESPACE, 
     ASTRA_DB_COLLECTION, 
+    ASTRA_DB_COLLECTION_ADMIN,
     ASTRA_DB_API_ENDPOINT, 
     ASTRA_DB_APPLICATION_TOKEN, 
     AI_API_KEY } = process.env;
